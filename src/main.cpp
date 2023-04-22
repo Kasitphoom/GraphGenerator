a188ee63428878e04d2753c3b2373405bd82abd3
Phupa Denphatcharangkul <earthphupa@gmail.com>
2023-04-22 23:15:11 +0700

    Added readDataFromCSV

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,55 @@
 #include<iostream>
+#include<fstream>
+#include<sstream>
 #include<string>
-#include <vector>
+#include<vector>
 
-struct data{
+
+struct Data{
     std::string name;
-    double value;
+    std::string value;
 };
 
 class Category{
     private:
         std::string name;
-        double value;
+        std::vector<Data> datas;
+        
+    public:
+        Category(std::string name,std::vector<Data> datas): name(name), datas(datas){};
+        
+
+        
 };
 
 class BarGraph{
     private:
         std::vector<Category> data;
 };
+
+//functions
+
+
+std::vector<std::string> readDataFromCSV(const std::string& filename) {
+    std::ifstream file(filename);
+    std::vector<std::string> lines;
+
+    // loop over each line of the file and add it to the vector
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
+
+int main() {
+    std::vector<std::string> lines = readDataFromCSV("../data.csv");
+
+    // print out the lines
+    for (const auto& line : lines) {
+        std::cout << line << std::endl;
+    }
+
+    return 0;
+}
