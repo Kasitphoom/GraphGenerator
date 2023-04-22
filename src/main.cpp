7a1223a4c2686d44a32f9f734e8ba931f7054a31
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-04-22 22:09:03 +0700

    add classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
 #include<iostream>
 #include<string>
 #include <vector>
+
+struct data{
+    std::string name;
+    double value;
+};
+
+class Category{
+    private:
+        std::string name;
+        double value;
+};
+
+class BarGraph{
+    private:
+        std::vector<Category> data;
+};
