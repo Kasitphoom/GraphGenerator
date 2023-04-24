2331eed7b5158970368fdeac9932e81ce9b22547
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-04-24 14:24:44 +0700

    add test case

---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "..\lib\doctest\doctest.h"
+#include "../src/graphgen.hpp"
+
+std::vector<std::string> readDataFromCSV(const std::string& filename) {
+    std::ifstream file(filename);
+    std::vector<std::string> lines;
+
+    // loop over each line of the file and add it to the vector
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
+
+TEST_CASE("Testing the readDataFromCSV function") {
+    std::vector<std::string> lines = readDataFromCSV("testdata.csv");
+    CHECK(lines == std::vector<std::string> {"HEADER", "month, money spent", "jan, 150", "feb, 200"});
+
+    Classified_Info classified_info(lines);
+    CHECK(classified_info.getName() == "HEADER");
+    CHECK(classified_info.getXAxis() == "month");
+    CHECK(classified_info.getYAxis() == "money spent");
+    CHECK(classified_info.getDatas() == std::vector<Data> {Data{"jan", 150}, Data{"feb", 200}});
+    CHECK(classified_info.getDatasMap() == std::map<std::string, double> {{"jan", 150}, {"feb", 200}});
+}
