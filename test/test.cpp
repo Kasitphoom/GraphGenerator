eab55dd670afdde8925e365e384b1ce24796d4a4
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-04-24 15:58:37 +0700

    add test case

--- test/test.cpp
+++ test/test.cpp
@@ -17,12 +17,25 @@ std::vector<std::string> readDataFromCSV(const std::string& filename) {
 
 TEST_CASE("Testing the readDataFromCSV function") {
     std::vector<std::string> lines = readDataFromCSV("testdata.csv");
-    CHECK(lines == std::vector<std::string> {"HEADER", "month, money spent", "jan, 150", "feb, 200"});
+    CHECK(lines == std::vector<std::string> {"HEADER", "month, money spent", "jan, 150", "jan, 200"});
 
     Classified_Info classified_info(lines);
     CHECK(classified_info.getName() == "HEADER");
     CHECK(classified_info.getXAxis() == "month");
     CHECK(classified_info.getYAxis() == "money spent");
-    CHECK(classified_info.getDatas() == std::vector<Data> {Data{"jan", 150}, Data{"feb", 200}});
-    CHECK(classified_info.getDatasMap() == std::map<std::string, double> {{"jan", 150}, {"feb", 200}});
+    CHECK(classified_info.getDatas() == std::vector<Data> {Data{"jan", 150}, Data{"jan", 200}});
+    CHECK(classified_info.getDatasMap() == std::map<std::string, double> {{"jan", 350}});
+}
+
+TEST_CASE("Checking Bargraph Correctness"){
+    std::vector<std::string> lines = readDataFromCSV("testdata.csv");
+    Classified_Info classified_info(lines);
+    BarGraph bargraph(classified_info.getDatasMap(), classified_info.getName(), classified_info.getXAxis(), classified_info.getYAxis());
+
+    CHECK(bargraph.getData() == std::map<std::string, double> {{"jan", 350}});
+    CHECK(bargraph.getTitle() == "HEADER");
+    CHECK(bargraph.getXLabel() == "month");
+    CHECK(bargraph.getYLabel() == "money spent");
+    CHECK(bargraph.getWidth() == 120);
+    CHECK(bargraph.getAxisValues() == std::vector<double> {0, 35, 70, 105, 140, 175, 210, 245, 280, 315, 350});
 }
