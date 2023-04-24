da60226c637a3f05c29e33b6cced8fa6f28011fc
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-04-24 15:58:22 +0700

    make somefucntion to BarGraph Class

--- src/graphgen.hpp
+++ src/graphgen.hpp
@@ -107,7 +107,103 @@ public:
 class BarGraph
 {
 private:
-    std::vector<Category> data;
+    std::map<std::string, double> data;
+
+    std::string title;
+    std::string xLabel;
+    std::string yLabel;
+
+    double width;
+    double height;
+    double* hlinePts;
+    double* vlinePts;
+    double* xLablePts;
+    double* yLablePts;
+
+    std::vector<double> axisValues;
+
+public:
+    BarGraph(const std::map<std::string, double> &data, std::string title, std::string xLable, std::string yLable)
+    {
+        this->data = data;
+        this->height = 600;
+        this->title = title;
+        this->xLabel = xLable;
+        this->yLabel = yLable;
+        calcWidth();
+        CalculateAxisValue();
+        hlinePts = new double[4];
+        vlinePts = new double[4];
+        xLablePts = new double[2];
+        yLablePts = new double[2];
+    }
+    BarGraph() = delete;
+    ~BarGraph(){
+        delete[] hlinePts;
+        delete[] vlinePts;
+        delete[] xLablePts;
+        delete[] yLablePts;
+    }
+
+    double find_max_value(){
+        double max = 0;
+        for (const auto &data : this->data){
+            if (data.second > max){
+                max = data.second;
+            }
+        }
+        return max;
+    }
+
+    void CalculateAxisValue(){
+        std::vector<double> axisValues;
+        double max = find_max_value();
+        double step = max / 10;
+        for (int i = 0; i < 11; i++){
+            axisValues.push_back(step * i);
+        }
+        this->axisValues = axisValues;
+    }
+
+    std::vector<double> getAxisValues() const
+    {
+        return axisValues;
+    }
+
+    void calcWidth(){
+        this->width = 60 * (data.size() + 1);
+    }
+
+    std::string getTitle() const
+    {
+        return title;
+    }
+
+    std::string getXLabel() const
+    {
+        return xLabel;
+    }
+
+    std::string getYLabel() const
+    {
+        return yLabel;
+    }
+
+    double getWidth() const
+    {
+        return width;
+    }
+
+    double getHeight() const
+    {
+        return height;
+    }
+
+    std::map<std::string, double> getData() const
+    {
+        return data;
+    }
+
 };
 
 #endif
